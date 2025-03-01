53da258343da89ca46732738fa68768512c9d22e
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-03-01 07:17:59 +0200

    feat(user):add `findById , deleteById` methods and rename save to create as a refactor

--- server/src/models/User.php
+++ server/src/models/User.php
@@ -1,37 +1,48 @@
 <?php
 require_once("Base.php");
 
-Class UserModel extends BaseModel{
+Class UserModel extends BaseModel
+{
     public $name;
-    public $email;  
+    public $email;
     public $password;
     public $phone_number;
     public $ceated_date;
     public $role;
 
-    public function __construct(
-        string $name,
-        string $email,
-        string $password,
-        string $phone_number,
-        string $role,
-    ) {
+    public function __construct()
+    {
         parent::__construct();
+    }
+
+    public function create(string $name, string $email, string $password, string $phone_number, string $role): void
+    {
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
         $this->phone_number = $phone_number;
         $this->role = $role;
-    }
-    function  save() {
-        $stmt  = $this->conn->prepare("INSERT INTO users (name,email,password,phone_number,role) VALUES (:name,:email,:password,:phone_number,:role)");
+
+        $stmt = $this->conn->prepare("INSERT INTO users (name,email,password,phone_number,role) VALUES (:name,:email,:password,:phone_number,:role)");
         $stmt->execute([
             ':name' => $this->name,
             ':email' => $this->email,
-            ':password' => $this->password,
+            ':password' => password_hash($this->password, PASSWORD_DEFAULT),
             ':phone_number' => $this->phone_number,
             ':role' => $this->role
         ]);
-        
     }
+
+    public function findById(int $id): array
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE id = :id");
+        $stmt->execute([':id' => $id]);
+        return $stmt->fetch();
+    }
+    public function deleteById(int $id): void
+    {
+        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = :id");
+        $stmt->execute([':id' => $id]);
+    }
+   
 }
