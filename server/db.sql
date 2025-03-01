8a766aa83250ced84edda67501a50a58ce59e65c
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-03-01 06:43:12 +0200

    feat(db): add initial SQL schema for database

---
+++ server/db.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    phone_number VARCHAR(255),
+    created_date VARCHAR(255),
+    role VARCHAR(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS sessions(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data JSON
+);
+
+
+CREATE TABLE IF NOT EXISTS categories(  
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS products(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    category_id INT,
+    price DECIMAL(10, 2),
+    stock_quantity INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE IF NOT EXISTS product_images(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT REFERENCES products(id),
+    image_url VARCHAR(255),
+    is_main BOOLEAN
+)
+
+
