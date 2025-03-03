6a1748b68a9109bd2e5e81c234f52d1bbcb724b3
rimex3 <faresahmed51857@gmail.com>
2025-03-03 23:14:26 +0200

    feat(auth): implement sign-up and sign-in components with validation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/router.tsx'
+import MainRootLayout from './components/MainRootLayout/MainRootLayout.tsx'
 
 const queryClient = new QueryClient()
 
@@ -10,7 +12,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MainRootLayout>
+        <RouterProvider router={router} />
+      </MainRootLayout>
     </QueryClientProvider>
   </StrictMode>,
 )
