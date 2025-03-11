6cac775508291526f10f9654a8071cb0f16fc210
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-03-11 20:44:08 +0200

    feat(favourite): Add favourite database schema and model

---
+++ server/src/models/Favourite.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . "/Base.php";
+class FavouriteModel extends BaseModel
+{
+    public $table = 'favourite';
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function addToFav(int $userId, int $productId)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO $this->table (user_id,product_id) VALUES (:userid,:productid)");
+        $stmt->execute([
+            ':userid' => $userId,
+            ':productid' => $productId
+        ]);
+    }
+    public function removeFromFav(int $userId, int $productId)
+    {
+        $stmt = $this->conn->prepare("DELETE FROM $this->table WHERE user_id = :userid join product on product.id = :productid");
+        $stmt->execute([
+            ':userid' => $userId,
+            ':productid' => $productId
+        ]);
+    }
+
+
+    public function getFav(int $userId)
+    {
+        $stmt  = $this->conn->prepare("SELECT * FROM $this->table JOIN products on products.id = $this->table.product_id WHERE user_id = :userid");
+        $stmt->execute([':userid' => $userId]);
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+}
